2440377ee457abb491eac6d211964e3c1c02eee9
Bhupendra Tripathi <bhupendratripathi.999@gmail.com>
2024-02-20 19:10:50 +0530

    First Commit...

---
+++ DependencyInjection/src/main/java/com/spring/Beans/Student.java
@@ -0,0 +1,11 @@
+package com.spring.Beans;
+
+public class Student {
+
+	private int sId;
+	private String studentName;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private String password;
+}
